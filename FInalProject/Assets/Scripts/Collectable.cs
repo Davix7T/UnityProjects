067535f8b21cf6b58b4de1fa4ad417385298ae72
Davix7T <davidstiven.ry@gmail.com>
2025-05-17 15:51:47 -0500

    coins
    cambio en el sonido para las monedas

--- FInalProject/Assets/Scripts/Collectable.cs
+++ FInalProject/Assets/Scripts/Collectable.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     [SerializeField] private CoinCounter coinCounter;
+    [SerializeField] private AudioClip collectSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // Aumenta el contador
             if (coinCounter != null)
             {
                 coinCounter.AddCoin();
             }
 
-            Destroy(this.gameObject);
+            // Reproduce el sonido en el mundo (incluso si el objeto se destruye)
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            }
+
+            // Destruye la moneda de inmediato
+            Destroy(gameObject);
         }
     }
 }
